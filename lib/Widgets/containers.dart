2566198dadc4c1f26fadf3b0ab47b00a2de66a3b
La Muerta <johnkhyle.siarot.20@usjr.edu.ph>
2023-04-21 01:04:51 +0800

    first commit

---
+++ lib/Widgets/containers.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:home/Widgets/container_card.dart';
+
+import '../data.dart';
+
+class Containerz extends StatefulWidget {
+  const Containerz({super.key});
+
+  @override
+  State<Containerz> createState() => _ContainerzState();
+}
+
+class _ContainerzState extends State<Containerz> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: 
+          volunteeringEvents.where((element) => element['organizationType'] != 'Government').map((e)=>  Padding(
+            padding: const EdgeInsets.symmetric(horizontal:8.0),
+            child: ContainerCard(lists: e['organizationName']),
+          )).toList()   
+    
+        
+      ),
+    );
+  }
+}
